f8e27eb3a185872d47ed12684db5660ecf7526d0
mavidser <mavidser@gmail.com>
2014-08-15 15:51:43 +0530

    Grabs the Problem code

--- codechef.py
+++ codechef.py
@@ -1,15 +1,25 @@
-#JUNE14
-
 import sublime, sublime_plugin
+import re
+import urllib
+import urllib2
+import threading
 
 class CodechefCommand(sublime_plugin.TextCommand):
+  def upload_solution(problem_code):
+    print problem_code
+
   def run(self, edit):
     file_name = self.view.file_name()
     filetype =  file_name[file_name.rfind('.')+1:]
+    line = self.view.substr(self.view.line(0))
     if filetype == 'py':
-      problem_code = self.view.find(r'#\w*$', 0, sublime.IGNORECASE)
-      problem_code = self.view.substr(problem_code)[1:]
-    elif filetype == 'cpp':
-      problem_code = self.view.find(r'//\w*$', 0, sublime.IGNORECASE)
-      problem_code = self.view.substr(problem_code)[2:]
-    print problem_code
+      try:
+        problem_code = re.search('#\s*\w*\s*$',line).string.strip()[1:].strip()
+        upload_solution(problem_code)
+      except AttributeError:
+        problem_code = self.view.window().show_input_panel('Enter the Problem Code:', 'ffs',upload_solution,None,None)
+    elif filetype == 'cpp' or filetype == 'c':
+      try:
+        problem_code = re.search('//\s*\w*\s*$', line).string.strip()[2:].strip()
+      except AttributeError:
+        problem_code = self.view.window().show_input_panel('Enter the Problem Code:', 'ffs',upload_solution,None,None)
